c8fa39260dbcd30893c92ed4c70b87c2089a407c
jorgefrcunha <109683766+jorgefrcunha@users.noreply.github.com>
2022-07-21 10:00:24 +0000

    submeter

---
+++ login1/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LOGIN</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="estilo.css">
+    </head>
+    <body>
+        <div>
+            <h1>Login</h1>
+            <input type="text" placeholder="Nome">
+            <br><br>
+            <input type="password" placeholder="Senha">
+            <br><br>
+            <button> Enviar </button>
+
+
+        </div>
+
+
+
+    </body>
+</html>
+
+
+
+
+
---
+++ login1/index1.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LOGIN</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="estilo_1.css">
+    </head>
+    <body>
+
+        <h1>Painel de Login administrativo</h1>
+        <div class="caixalogin">
+            <div class="img-user">
+                <img src="img/login.png">  
+            </div>
+          
+    
+        <h2> Efetuar login do sistema</h2>
+        <form>
+            <div class="campo">
+                <input type="text" required="">
+                <label>Utilizador</label>
+            </div>
+            <div class="campo">
+                <input type="password" required="">
+                <label> Senha</label>
+            </div>
+            <input type="checkbox" checked="checked" class="checkbox">
+
+            <label class="checkbox"> Lembrar </label><br><br>
+
+
+            <button> Enviar </button>
+            <p id="registar"> Efetuar Registo</p>
+
+        </form>
+    </div>
+
+    <div><h2></h2> </div>
+
+</body>
+</html>
+
+
+
+
+
